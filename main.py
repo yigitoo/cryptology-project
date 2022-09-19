58bef8b63eecd6bcaa1a1acf3987c05c84e8f244
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2022-09-19 16:13:47 +0300

    morse encoder, decoder

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-from cipher import (
+from mitcipher import (
     Decrypt,
     Encrypt
 )
 import tkinter as tk
 import threading
 from functools import partial
+import pymongo as mongo
+
 
 #variables and funcs
-def solver(path = None):
+def decipher(path = None):
     Decrypt(path)
 def cipher(path = None):
     Encrypt(path)
 
 #GUI
 root = tk.Tk()
-frame = tk.Frame(bg='#32334f')
+root.geometry("400x400")
+root.title("Kriptoloji Projesi")
 
-cipher_label = tk.Label(text = "To encypt your data give the text", fg = "#3d0192")
+frame = tk.Frame(bg='#323340')
 
+cipher_label = tk.Label(
+    frame, 
+    text = "To encypt your data give the text",
+    fg = "#3d0192"
+)
 
+cipher_label.grid(row=0, column=1)
+frame.pack()
 root.mainloop()
